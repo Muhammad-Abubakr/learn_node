189f59dd447587222d1da56c547f33ecb846cf72
Muhammad-Abubakr <kmabs36@gmail.com>
2023-01-30 22:55:46 +0500

    included headers and more methods and functionality in http-module

--- 11-http-module.js
+++ 11-http-module.js
@@ -1,22 +1,51 @@
-const http = require('http')
+const http = require('http');
+
+const PORT = 5000;
 
 const server = http.createServer((req, res) => {
+    console.log('User hit the server');
 
-    if (req.url === '/') {
-        res.write('Welcome');
-    } else if (req.url === '/about') {
-        res.write('About Page');
-    } else {
-        res.write(`
-        <h1>Oops!</h1>
-        <p> Looks like you are lost... </p>
-        <a href="/">Go back to the homepage</a>
-        `);
-    }
+    console.log('Preparing response...');
+    console.log('Setting up response headers...');
+    res.statusCode = 200;
+    res.statusMessage = 'OK';
+    res.setHeader('statusCode', 200);
+    res.setHeader('statusMessage', 'OK');
+    res.setHeader('content-type', 'text/html')
+    res.setHeader('customHeader', 'more info');
+
+    console.log('Sending data to user...');
+    res.write(`<h1>${req.url}</h1>`);
+    console.log('Data sent...');
 
     res.end();
-})
+    console.log('Response end');
+    console.log('============');
+});
+
+server.listen(PORT);
+console.log(`Listening on Port ${PORT}...`);
 
 
-server.listen(1025)
-console.log('listening on port 1025...')
+// const http = require('http')
+
+// const server = http.createServer((req, res) => {
+
+//     if (req.url === '/') {
+//         res.write('Welcome');
+//     } else if (req.url === '/about') {
+//         res.write('About Page');
+//     } else {
+//         res.write(`
+//         <h1>Oops!</h1>
+//         <p> Looks like you are lost... </p>
+//         <a href="/">Go back to the homepage</a>
+//         `);
+//     }
+
+//     res.end();
+// })
+
+
+// server.listen(1025)
+// console.log('listening on port 1025...')
